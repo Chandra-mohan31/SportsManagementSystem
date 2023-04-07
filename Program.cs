89fc837b33a288b91cc50e0c5f58c6bc94ea31f4
unknown <chandrashanmugam007@gmail.com>
2023-04-07 14:37:13 +0530

    added menu for interaction

--- Program.cs
+++ Program.cs
@@ -72,6 +72,7 @@ namespace SportsManagementSystem
    
         public static void AddTournament(string tournamentName, int sportId, string tournamentDate, SqlConnection conn)
         {
+            
             SqlCommand cmd = conn.CreateCommand();
 
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
@@ -136,6 +137,55 @@ namespace SportsManagementSystem
             cmd.CommandText = $"RemoveTournament";
             cmd.ExecuteReader().Close();
         }
+
+        public static void GetSportsAvailable(SqlConnection conn)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from SportsTable";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //for(int i=0;i<reader.FieldCount;i++) {
+                //    Console.Write($"{reader[i]} \t");
+                //}
+              
+                Console.WriteLine(reader.GetInt32(0) + "| " + reader.GetString(1));
+            }
+            reader.Close();
+        }
+        public static void GetScoreCard(SqlConnection conn)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from ScoreCard";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i]} \t");
+                }
+
+                Console.WriteLine();
+            }
+            reader.Close();
+        }
+        public static void GetTableData(SqlConnection conn,string tableName)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = $"select * from {tableName}";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i]} \t");
+                }
+
+                Console.WriteLine();
+            }
+            reader.Close();
+        }
+        //associate tournament id to score card
         static void Main(string[] args)
         {
             string connString = "Data Source=F48DPF2;Initial Catalog=SportsManagementSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
@@ -143,7 +193,6 @@ namespace SportsManagementSystem
 
             SqlConnection conn = new SqlConnection(connString);
             conn.Open();
-
             //SportsManagementSystem(conn);
             //AddSports("Volley Ball", 6, conn);
             //AddScoreCard(3, "t1", "t2", 120, 130, "t3", conn);
@@ -154,7 +203,152 @@ namespace SportsManagementSystem
 
             Console.WriteLine("Collge Sports Management System:");
             Console.WriteLine("Menu: ");
-            RemoveTournament(4,conn);
+
+
+
+
+            
+
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Add sport");
+                Console.WriteLine("2. Add scorecard");
+                Console.WriteLine("3. Add tournament");
+                Console.WriteLine("4. Remove sport");
+                Console.WriteLine("5. Edit scorecard");
+                Console.WriteLine("6. Remove player");
+                Console.WriteLine("7. Remove tournament");
+                Console.WriteLine("8. Exit");
+
+                int option = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter sport name:");
+                            string sportName = Console.ReadLine();
+
+                            Console.WriteLine("Enter number of players:");
+                            int numberOfPlayers = int.Parse(Console.ReadLine());
+
+                            AddSports(sportName, numberOfPlayers, conn);
+                            Console.WriteLine("Sport added successfully");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Available Sports data: ");
+                            GetSportsAvailable(conn);
+
+                            Console.WriteLine("Enter sport ID:");
+                            int sportId = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter team 1:");
+                            string team1 = Console.ReadLine();
+
+                            Console.WriteLine("Enter team 2:");
+                            string team2 = Console.ReadLine();
+
+                            Console.WriteLine("Enter team 1 score:");
+                            int team1Score = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter team 2 score:");
+                            int team2Score = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter winner:");
+                            string winner = Console.ReadLine();
+
+                            AddScoreCard(sportId, team1, team2, team1Score, team2Score, winner, conn);
+                            Console.WriteLine("Scorecard added successfully");
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Available Sports data: ");
+
+                            GetSportsAvailable(conn);
+
+                            Console.WriteLine("Enter tournament name:");
+                            string tournamentName = Console.ReadLine();
+
+                            Console.WriteLine("Enter sport ID:");
+                            int tournamentSportId = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter tournament date (MM/DD/YYYY):");
+                            string tournamentDate = Console.ReadLine();
+
+                            AddTournament(tournamentName, tournamentSportId, tournamentDate, conn);
+                            Console.WriteLine("Tournament added successfully");
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Available Sports data: ");
+
+                            GetSportsAvailable(conn);
+                            Console.WriteLine("Enter sport ID:");
+                            int removeSportId = int.Parse(Console.ReadLine());
+
+                            RemoveSport(removeSportId, conn);
+                            Console.WriteLine("Sport removed successfully");
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Current Score Card:");
+                            GetScoreCard(conn);
+                            Console.WriteLine("Enter scorecard ID:");
+                            int scoreCardId = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter team 1 score:");
+                            int editTeam1Score = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter team 2 score:");
+                            int editTeam2Score = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter winner:");
+                            string editWinner = Console.ReadLine();
+
+                            EditScoreCard(scoreCardId, editTeam1Score, editTeam2Score, editWinner, conn);
+                            Console.WriteLine("Scorecard edited successfully");
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Players Availability:");
+                            GetTableData(conn,"Players");
+
+                            Console.WriteLine("Enter player ID:");
+                            int removePlayerId = int.Parse(Console.ReadLine());
+
+                            RemovePlayer(removePlayerId, conn);
+                            Console.WriteLine("Player removed successfully");
+                            break;
+
+                        case 7:
+                            Console.WriteLine("Tournaments Around:");
+                            GetTableData(conn, "Tournament");
+                            Console.WriteLine("Enter tournament ID:");
+                            int removeTournamentId = int.Parse(Console.ReadLine());
+
+                            RemoveTournament(removeTournamentId, conn);
+                            Console.WriteLine("Tournament removed successfully");
+                            break;
+
+                        case 8:
+                            return;
+
+                        default:
+                            Console.WriteLine("Invalid option");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+
+
+
             conn.Close();
         }
     }
